23fd439aff2d0dd508a8f5da4da62eb9a17ae56a
Buddhabhushan Sirsat <96968532+buddhabhushan22110329@users.noreply.github.com>
2022-10-11 20:50:52 +0530

    Add files via upload

---
+++ Drum Kit/script.js
@@ -0,0 +1,33 @@
+var w = new Audio("sounds/tom-1.mp3")
+var a = new Audio("sounds/tom-2.mp3")
+var s = new Audio("sounds/tom-3.mp3")
+var d = new Audio("sounds/tom-4.mp3")
+var j = new Audio("sounds/snare.mp3")
+var k = new Audio("sounds/crash.mp3")
+var l = new Audio("sounds/kick-bass.mp3")
+
+// var wclass = document.getElementById('wID');
+
+
+function wp(){
+    // wclass.classList.add("change");
+    w.play();
+}
+function ap(){
+    a.play();
+}
+function sp(){
+    s.play();
+}
+function dp(){
+    d.play();
+}
+function jp(){
+    j.play();
+}
+function kp(){
+    k.play();
+}
+function lp(){
+    l.play();
+}
